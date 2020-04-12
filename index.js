5c61a8292ffc9edb591d0a550c03df4e2a66bdc1
naoto86 <memento.mori.1986@gmail.com>
2020-04-12 23:41:04 +0900

    react-native-navigationの初期設定

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-import Storybook from './storybook/';
-export default Storybook;
+/**
+ * @format
+ */
+import {Navigation} from 'react-native-navigation';
+import App from './App';
+import {name as appName} from './app.json';
+
+Navigation.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: appName,
+            },
+          },
+        ],
+      },
+    },
+  });
+});
